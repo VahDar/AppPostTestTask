232704c7868809d3eaee87130e69ff307d30e264
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-02-26 11:44:06 +0200

    create Coordinator protocol

--- AppPostTestTask/Coordinator/Coordinator.swift
+++ AppPostTestTask/Coordinator/Coordinator.swift
@@ -6,8 +6,37 @@
 //
 
 import Foundation
+import UIKit
 
 protocol Coordinator: AnyObject {
     
     var childCoordinator: [Coordinator] { get set }
+    var parentCoordinator: Coordinator? { get set }
+    var navigationController: UINavigationController? { get }
+    func start()
+    func finish()
+    func addChildCoordinator(_ coordinator: Coordinator)
+    func removeChildCoordinator(_ coordinator: Coordinator)
+    func removeAllChildCoordinatorWith<T>(type: T.Type)
+    func removeAllChildCiirdunatir()
+}
+
+extension Coordinator {
+    
+    func addChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinator.append(coordinator)
+    }
+    
+    func removeChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinator = childCoordinator.filter { $0 !== coordinator}
+    }
+    
+    func removeAllChildCoordinatorWith<T>(type: T.Type) {
+        childCoordinator = childCoordinator.filter { $0 is T == false }
+    }
+    
+    func removeAllChildCiirdunatir() {
+        childCoordinator.removeAll()
+    }
+    
 }
