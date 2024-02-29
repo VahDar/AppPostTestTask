309237c393959781721b2150e95b80e7763fd52e
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-02-29 16:30:41 +0200

    add func getDetail

--- AppPostTestTask/Network/NetworkService.swift
+++ AppPostTestTask/Network/NetworkService.swift
@@ -9,15 +9,27 @@ import Foundation
 
 protocol NetworkServiceProtocol {
     func getPost() async throws -> PostsListModel
+    func getDetail(id: String) async throws -> DetailScreenModel
 }
 
 final class NetworkService: NetworkHTTPClient, NetworkServiceProtocol {
+    
+    
     
     func getPost() async throws -> PostsListModel {
         do {
             return try await fetchData(
                 from: Endpoint.list.path, requestMethod: RequestMethod.get,
                 responseType: PostsListModel.self)
+        } catch {
+            print("Error fetching data: \(error.localizedDescription)")
+            throw error
+        }
+    }
+    
+    func getDetail(id: String) async throws -> DetailScreenModel {
+        do {
+            return try await fetchData(from: Endpoint.detail(id: id).path, requestMethod: RequestMethod.get, responseType: DetailScreenModel.self)
         } catch {
             print("Error fetching data: \(error.localizedDescription)")
             throw error
